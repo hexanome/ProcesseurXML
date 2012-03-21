9782634069ce1c8c6074746b695f1020d65e4999
mcoquet <matthieu.coquet@insa-lyon.fr>
2012-03-21 13:57:26 +0100

    Worked on unit tests
    Signed-off-by: mcoquet <matthieu.coquet@insa-lyon.fr>

--- Analyseur/dtd/model/test.cpp
+++ Analyseur/dtd/model/test.cpp
@@ -1,38 +1,38 @@
+#include "../../../Test/test.h"
 #include "../../includes/common.h"
 #include "Element.h"
 #include "Attribut.h"
 #include "Sequence.h"
-// ..mettre les autres includes.
-#include "../../../Test/test.h"
 
+// Testing DTD services and serialization
 
 bool serializeElementCategory() {
-	Element* e = new Element("note");
-	e->setCategory("ANY");
-	string xml = e->serialize();
-	delete e;
-	cout<<endl<<xml<<endl;
-	return xml == "<!ELEMENT note ANY>";	
+  Element* e = new Element("note");
+  e->setCategory("ANY");
+  string xml = e->serialize();
+  delete e;
+  cout<<endl<<xml<<endl;
+  return xml == "<!ELEMENT note ANY>";  
 }
 
 bool serializeElementComplexe() {
-	Element* e = new Element("note");
-	Sequence* s = new Sequence();
-	s->addContenuCompose(new Element("to","(#PCDATA)"));
-	s->addContenuCompose(new Element("from","(#PCDATA)"));
-	s->addContenuCompose(new Element("heading","(#PCDATA)"));
-	s->addContenuCompose(new Element("body","(#PCDATA)"));
-	e->setSerie(s);	
-	string xml = e->serialize();
-	cout<<endl<<xml<<endl;
-	delete e;
-	return xml == "<!ELEMENT note (to,from,heading,body)><!ELEMENT to (#PCDATA)><!ELEMENT from (#PCDATA)><!ELEMENT heading (#PCDATA)><!ELEMENT body (#PCDATA)>";	
+  Element* e = new Element("note");
+  Sequence* s = new Sequence();
+  s->addContenuCompose(new Element("to","(#PCDATA)"));
+  s->addContenuCompose(new Element("from","(#PCDATA)"));
+  s->addContenuCompose(new Element("heading","(#PCDATA)"));
+  s->addContenuCompose(new Element("body","(#PCDATA)"));
+  e->setSerie(s);  
+  string xml = e->serialize();
+  cout<<endl<<xml<<endl;
+  delete e;
+  return xml == "<!ELEMENT note (to,from,heading,body)><!ELEMENT to (#PCDATA)><!ELEMENT from (#PCDATA)><!ELEMENT heading (#PCDATA)><!ELEMENT body (#PCDATA)>";  
 }
 
 bool serializeElementMegaComplexe() {
-	Element* e = new Element("TVSCHEDULE");
+  Element* e = new Element("TVSCHEDULE");
     Element* c = new Element("CHANNEL");
-	Sequence* s = new Sequence();
+  Sequence* s = new Sequence();
     c->setCardinalite("+");
     s->addContenuCompose(c);
     e->setSerie(s);
@@ -40,24 +40,26 @@ bool serializeElementMegaComplexe() {
    
     Sequence* s2 = new Sequence();    
     s2->addContenuCompose(new Element("to","(#PCDATA)"));
-	s2->addContenuCompose(new Element("from","(#PCDATA)"));
+  s2->addContenuCompose(new Element("from","(#PCDATA)"));
     c->setSerie(s2);
     c->addAttribut(new Attribut("CHANNEL","CHAN"));
     e->addAttribut(new Attribut("TVSCHEDULE","NAME"));
     
-	string xml = e->serialize();
-	cout<<endl<<xml<<endl;
-	delete e;
-	return xml == "<!ELEMENT TVSCHEDULE (CHANNEL)><!ELEMENT CHANNEL (to,from)><!ELEMENT to (#PCDATA)><!ELEMENT from (#PCDATA)><!ATTLIST CHANNEL CHAN CDATA #IMPLIED><!ATTLIST TVSCHEDULE NAME CDATA #IMPLIED>";	
+  string xml = e->serialize();
+  cout<<endl<<xml<<endl;
+  delete e;
+  return xml == "<!ELEMENT TVSCHEDULE (CHANNEL)><!ELEMENT CHANNEL (to,from)><!ELEMENT to (#PCDATA)><!ELEMENT from (#PCDATA)><!ATTLIST CHANNEL CHAN CDATA #IMPLIED><!ATTLIST TVSCHEDULE NAME CDATA #IMPLIED>";  
 }
 
 bool serializeAttList() {
-	Attribut* a = new Attribut("elementTest","taille");
-	string xml = a->serialize();
-	delete a;
+  Attribut* a = new Attribut("elementTest","taille");
+  string xml = a->serialize();
+  delete a;
     cout<<endl<<xml<<endl;
-	return xml == "<!ATTLIST elementTest taille CDATA #IMPLIED>";	
+  return xml == "<!ATTLIST elementTest taille CDATA #IMPLIED>";  
 }
+
+// Run the tests
 
 int main(int argc, char ** argv) {
   int passed = 0;
@@ -67,7 +69,7 @@ int main(int argc, char ** argv) {
   test->run("serializeAttribut", serializeAttList);
   test->run("serializeElementMegaComplexe", serializeElementMegaComplexe);
   test->end();
-	delete test;
+  delete test;
   return 0;
 }
 
@@ -84,9 +86,9 @@ int main(int argc, char ** argv) {
  <!ELEMENT TITLE (#PCDATA)> 
  <!ELEMENT DESCRIPTION (#PCDATA)>
  
- <!ATTLIST TVSCHEDULE	NAME CDATA #IMPLIED>
- <!ATTLIST CHANNEL	CHAN CDATA #IMPLIED>
- <!ATTLIST PROGRAMSLOT	 VTR CDATA #IMPLIED>
+ <!ATTLIST TVSCHEDULE  NAME CDATA #IMPLIED>
+ <!ATTLIST CHANNEL  CHAN CDATA #IMPLIED>
+ <!ATTLIST PROGRAMSLOT   VTR CDATA #IMPLIED>
  <!ATTLIST TITLE RATING CDATA #IMPLIED>
  <!ATTLIST TITLE LANGUAGE CDATA #IMPLIED> 
 */
