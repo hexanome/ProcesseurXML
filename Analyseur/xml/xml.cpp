1fd8282441beb309ddb40e919d48431985a6e059
Thaddee Tyl <thaddee.tyl@gmail.com>
2012-03-21 14:00:53 +0100

    asdf
    Signed-off-by: Thaddee Tyl <thaddee.tyl@gmail.com>

--- Analyseur/xml/xml.cpp
+++ Analyseur/xml/xml.cpp
@@ -1,9 +1,13 @@
 #include "xml.h"
 
+extern int xmllineno;
+
 Document* XmlParser::parseStream(FILE* fd)
 {
 	// Specify the stream to use by the parser.
 	xmlFlushBuffer();
+	//YY_FLUSH_BUFFER();
+	printf("LINENO: %d\n", xmllineno);
 	xmlin = fd;
 
 	// Start the parsing of the document.
