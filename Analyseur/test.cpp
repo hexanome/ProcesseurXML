d5f347132647a228de98e38fe7c16d24b2117e4a
Alexandre Lefoulon <alexandre.lefoulon@insa-lyon.fr>
2012-03-27 17:27:21 +0200

    test a verifier

--- Analyseur/test.cpp
+++ Analyseur/test.cpp
@@ -27,12 +27,20 @@ bool validateXmlDtd() {
   
 bool validateComplexDtd () {
 	
-	
 	// XML 
-  	TextNode *text = new TextNode("bonjour");
-  	ElementNode *element = new ElementNode("", "element");
+  	TextNode *text = new TextNode("maison");
+  	ElementNode *element = new ElementNode("CHANNEL", "to");
   	element->appendChild(text);	
-	
+  	
+  	ElementNode *element2 = new ElementNode("TVSCHEDULE", "CHANNEL");
+  	element2->appendChild(element);
+  	
+  	ElementNode *element3 = new ElementNode("", "TVSCHEDULE");
+  	element3->appendChild(element2);
+  	
+  	element3->setAttribute("NAME","honneur");
+  	element2->setAttribute("CHAN","32");
+  		
 	// DTD
 	Element* e = new Element("TVSCHEDULE");
     Element* c = new Element("CHANNEL");
@@ -47,7 +55,25 @@ bool validateComplexDtd () {
     c->setSerie(s2);
     c->addAttribut(new Attribut("CHANNEL","CHAN"));
     e->addAttribut(new Attribut("TVSCHEDULE","NAME"));
-    
+
+    // VALIDATE
+    bool valid = element3->isValid(e);
+  	delete text; delete element; delete element2; delete element3;
+  	return valid;    
+  	
+  	/*<!ELEMENT TVSCHEDULE (CHANNEL)>
+	<!ELEMENT CHANNEL (to,from)>
+	<!ELEMENT to (#PCDATA)>
+	<!ELEMENT from (#PCDATA)>
+	<!ATTLIST CHANNEL CHAN CDATA #IMPLIED>
+	<!ATTLIST TVSCHEDULE NAME CDATA #IMPLIED>
+
+	<TVSCHEDULE NAME="honneur">
+		<CHANNEL CHAN="32">
+			<to>maison</to>
+		</CHANNEL>
+	</TVSCHEDULE>*/
+
     
 }
 // Run the tests
